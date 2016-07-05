78a607536ec76385e2be00fc3c19e560c2f551ea
Mike Little <michaell@olympic.co.nz>
2016-07-05 17:57:14 +1200

    experiments with styling

--- src/app/+root/root.component.ts
+++ src/app/+root/root.component.ts
@@ -4,7 +4,8 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
 @Component({
     moduleId: module.id,
     directives: [ROUTER_DIRECTIVES],
-    selector: "root-component",
+    selector: 'root-component',
+    styleUrls: ['root.component.css'],
     templateUrl: 'root.component.html'
 })
 export class RootComponent {
