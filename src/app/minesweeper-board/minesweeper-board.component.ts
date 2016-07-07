2a084cb1703195b6c25938ff495dd92c1ffbaefa
Mike Little <michaell@olympic.co.nz>
2016-07-07 17:33:57 +1200

    started writing board generation code

--- src/app/minesweeper-board/minesweeper-board.component.ts
+++ src/app/minesweeper-board/minesweeper-board.component.ts
@@ -12,6 +12,25 @@ export class MinesweeperBoardComponent implements OnInit {
   @Input() gameConfig: GameConfig;
 
   ngOnInit() {
-    
+    //Build board
   } 
+}
+
+class MinesweeperBoard {
+
+  private tiles: Tile[][];
+
+  constructor(public numMines: number, public width: number, public height: number){
+    this.tiles = new Array<Array<Tile>>(width);
+  }
+
+  isTileMine(x : number, y: number) {
+    return this.tiles[x][y].isMine;
+  }
+}
+
+class Tile {
+  isRevealed = false;
+  isMine = false;
+  numAdjacentMines: number;
 }
