fc34bea00c5ae6827e0502c32d6fa7ac35fa31d7
Mike Little <michaell@olympic.co.nz>
2016-07-09 17:25:38 +1200

    extract model classes to shared folder

--- src/app/minesweeper-board/minesweeper-board.component.ts
+++ src/app/minesweeper-board/minesweeper-board.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { GameConfig } from '../+game';
 
+import { MinesweeperBoard } from '../shared'
+
 import * as _ from 'lodash';
 
 @Component({
@@ -13,33 +15,10 @@ export class MinesweeperBoardComponent implements OnInit {
 
   @Input() gameConfig: GameConfig;
 
+  board: MinesweeperBoard
+
   ngOnInit() {
     //Build board
+    this.board = new MinesweeperBoard(this.gameConfig.mines, this.gameConfig.width, this.gameConfig.height);
   }
-}
-
-class MinesweeperBoard {
-
-  private tiles: Tile[][];
-
-  constructor(public numMines: number, public width: number, public height: number){
-    this.tiles = _.map(_.range(width), i => _.map(_.range(height), j => new Tile()));
-
-    //populate mines
-    while(numMines > 0){
-      let x = Math.floor(Math.random() * width),
-          y = Math.floor(Math.random() * height);
-
-      if(!this.tiles[x][y].isMine){
-        this.tiles[x][y].isMine = true;
-        numMines--;
-      }
-    }
-  }
-}
-
-class Tile {
-  isRevealed = false;
-  isMine = false;
-  numAdjacentMines: number;
 }
---
+++ src/app/shared/minesweeper-board.ts
@@ -0,0 +1,25 @@
+export class MinesweeperBoard {
+
+  private tiles: Tile[][];
+
+  constructor(public numMines: number, public width: number, public height: number){
+    this.tiles = _.map(_.range(width), i => _.map(_.range(height), j => new Tile()));
+
+    //populate mines
+    while(numMines > 0){
+      let x = Math.floor(Math.random() * width),
+          y = Math.floor(Math.random() * height);
+
+      if(!this.tiles[x][y].isMine){
+        this.tiles[x][y].isMine = true;
+        numMines--;
+      }
+    }
+  }
+}
+
+export class Tile {
+  isRevealed = false;
+  isMine = false;
+  numAdjacentMines: number;
+}
